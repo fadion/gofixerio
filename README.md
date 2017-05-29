cea4cfab17c69636a597519e594b52c5918f0f9a
Fadion Dashi <jonidashi@gmail.com>
2017-05-29 23:17:45 +0200

    First commit
    Signed-off-by: Fadion Dashi <jonidashi@gmail.com>

---
+++ README.md
@@ -0,0 +1,63 @@
+# Thin wrapper for Fixer.io
+
+A thin wrapper in Go for [Fixer.io](http://www.fixer.io), a service for foreign exchange rates and currency conversion. It provides a few methods to easily construct the url, makes the api call and gives back the response.
+
+## Installation
+
+As for any other package, you can use `go get`:
+
+```
+$ go get github.com/fadion/gofixerio
+```
+
+For better package management however, I'd recommend [glide](https://github.com/Masterminds/glide) or the still in alpha [dep](https://github.com/golang/dep).
+
+## Usage
+
+First, let's import the package:
+
+```go
+import "github.com/fadion/gofixerio"
+```
+
+Let's see an exhaustive example with all the parameters:
+
+```go
+exchange := gofixerio.New()
+
+exchange.Base(gofixerio.EUR)
+exchange.Symbols(gofixerio.USD, gofixerio.AUD)
+exchange.Secure(true)
+exchange.Historical(time.Date(2016, time.December, 15, 0, 0, 0, 0, time.UTC))
+
+if rates, err := exchange.GetRates(); err == nil {
+    fmt.Println(rates[gofixerio.USD])
+}
+```
+
+Every parameter can be omitted as the package provides some sensible defaults. The base currency is `EUR`, makes a secure connection by default and returns all the supported currencies.
+
+## Response
+
+The response is a simple `map[string]float32` with currencies as keys and ratios as values. For a request like the following:
+
+```go
+exchange := gofixerio.New()
+exchange.Symbols(gofixerio.USD, gofixerio.BGP)
+
+rates, _ := exchange.GetRates()
+fmt.Println(rates)
+```
+
+the response will be the map:
+
+```go
+map[USD:1.1188 USD:0.87093]
+```
+
+which you can access with the keys as strings or using the currency constants:
+
+```go
+rates["EUR"];
+rates[gofixerio.GBP];
+```
---
+++ main.go
@@ -0,0 +1,135 @@
+package gofixerio
+
+import (
+    "strings"
+    "net/http"
+    "io/ioutil"
+    "encoding/json"
+    "time"
+    "bytes"
+    "errors"
+)
+
+type fixerio struct {
+    base     string
+    protocol string
+    date     string
+    symbols  []string
+}
+
+type response struct {
+    Base  string `json:"base"`
+    Date  string `json:"date"`
+    Rates rates `json:"rates"`
+}
+
+type rates map[string]float32
+
+var baseUrl = "api.fixer.io"
+
+// Initializes fixerio.
+func New() *fixerio {
+    return &fixerio{
+        base:     EUR,
+        protocol: "https",
+        date:     "",
+        symbols:  make([]string, 0),
+    }
+}
+
+// Set base currency.
+func (f *fixerio) Base(currency string) {
+    f.base = currency
+}
+
+// Make the connection secure or not by setting the
+// secure argument to true or false.
+func (f *fixerio) Secure(secure bool) {
+    if secure {
+        f.protocol = "https"
+    } else {
+        f.protocol = "http"
+    }
+}
+
+// List of currencies that should be returned.
+func (f *fixerio) Symbols(currencies ...string) {
+    f.symbols = currencies
+}
+
+// Specify a date in the past to retrieve historical records.
+func (f *fixerio) Historical(date time.Time) {
+    f.date = date.Format("2006-01-02")
+}
+
+// Retrieve the exchange rates.
+func (f *fixerio) GetRates() (rates, error) {
+    url := f.buildUrl()
+    body, err := f.makeRequest(url)
+
+    if err != nil {
+        return rates{}, err
+    }
+
+    response, err := f.parseJson(body)
+
+    if err != nil {
+        return rates{}, err
+    }
+
+    return response, nil
+}
+
+func (f *fixerio) buildUrl() string {
+    var url bytes.Buffer
+
+    url.WriteString(f.protocol)
+    url.WriteString("://")
+    url.WriteString(baseUrl)
+    url.WriteString("/")
+
+    if f.date == "" {
+        url.WriteString("latest")
+    } else {
+        url.WriteString(f.date)
+    }
+
+    url.WriteString("?base=")
+    url.WriteString(string(f.base))
+
+    if len(f.symbols) >= 1 {
+        url.WriteString("&symbols=")
+        url.WriteString(strings.Join(f.symbols, ","))
+    }
+
+    return url.String()
+}
+
+func (f *fixerio) makeRequest(url string) (string, error) {
+    response, err := http.Get(url)
+
+    if err != nil {
+        return "", errors.New("Couldn't connect to server")
+    }
+
+    defer response.Body.Close()
+    body, err := ioutil.ReadAll(response.Body)
+
+    if err != nil {
+        return "", errors.New("Couldn't parse response")
+    }
+
+    return string(body), nil
+}
+
+func (f *fixerio) parseJson(body string) (rates, error) {
+    var response response
+
+    err := json.Unmarshal([]byte(body), &response)
+
+    if err != nil {
+        return rates{}, errors.New("Couldn't parse response")
+    }
+
+    return response.Rates, nil
+}
